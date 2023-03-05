d5fbea3987a0ecd472a32675cf71186e1c67821f
gustavoacbrito <121517266+gustavoacbrito@users.noreply.github.com>
2023-03-05 20:32:35 -0300

    Update fatorial.js

--- fatorial.js
+++ fatorial.js
@@ -5,11 +5,13 @@ var resposta = document.querySelector("#resposta");
 
 botao.addEventListener("click",fatorial);
 
+
+
 function fatorial() {
 
     var n = document.getElementById("caixa_de_entrada").value;
 
-    if (isNaN(n) || (!Number.isInteger(Number(n))) || n<0){
+    if (isNaN(n) || (!Number.isInteger(Number(n))) || n<0 || !n)  {
       resultado = `${n} não é uma entrada válida. Valor inserido deve ser um número, inteiro e positivo.`; // string entre crase e com variavel dentro de ${}
       
     }
@@ -28,7 +30,6 @@ function fatorial() {
 
     
     
-
 
   
   
